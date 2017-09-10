b8520754e8b24342d7232a9f8cdc3aecaac03f7b
roo2 <roo2@protonmail.com>
2017-09-10 15:19:25 +1000

    remove gas limit and gas price to prevent this error https://github.com/ethereum/go-ethereum/issues/662

--- helpers/blockchain-helper.js
+++ helpers/blockchain-helper.js
@@ -40,13 +40,10 @@ module.exports = {
         if (!web3.isAddress(to)) {
           return reject({code: 500, title: 'Error', message: 'invalid address'})
         }
-        const gasPrice = parseInt(web3.eth.gasPrice)
         const txArgs = {
           from: config.get('ethereum.account'),
           to: to,
-          value: parseInt(web3.toWei(amountInEther, 'ether')),
-          gasLimit: config.get('ethereum.gasLimit'),
-          gasPrice: gasPrice
+          value: parseInt(web3.toWei(amountInEther, 'ether'))
         }
         log.verbose(`sending ${txArgs.value} wei from ${txArgs.from} to ${txArgs.to}`)
         web3.eth.sendTransaction(txArgs, (err, hash) => {
